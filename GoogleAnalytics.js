93590f58294705cf1afdcfb4951cb9ecf0c588fc
Kyle Paul <kpaul86+github@gmail.com>
2014-10-10 14:11:51 -0500

    Add logic to prevent debugging on production compiled code

--- GoogleAnalytics.js
+++ GoogleAnalytics.js
@@ -94,7 +94,11 @@ Ext.define('Ext.ux.GoogleAnalytics', {
             (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-            })(window,document,'script',me.debugMode ? '//www.google-analytics.com/analytics_debug.js' : '//www.google-analytics.com/analytics.js','ga');
+            })(window,document,'script',
+            //<debug>
+            me.debugMode ? '//www.google-analytics.com/analytics_debug.js' :
+            //</debug>
+            '//www.google-analytics.com/analytics.js','ga');
             
             ga('create', me.trackingCode, 'auto');
             if(me.forceSSL) {
